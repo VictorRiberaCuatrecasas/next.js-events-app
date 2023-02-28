26e2c381715e82237977c082a9567aae46919d15
Victor <victorribera92@gmail.com>
2023-02-28 17:12:16 +0100

    finished events browser with dummy data

--- components/ui/Button.jsx
+++ components/ui/Button.jsx
@@ -1,8 +1,14 @@
 import Link from "next/link";
-import styles from "./Button.module.css"
+import styles from "./Button.module.css";
 
 export default function Button(props) {
-  return (
-    <Link className={styles.btn} href={props.link}>{props.children}</Link>
-  );
+  if (props.link) {
+    return (
+      <Link className={styles.btn} href={props.link}>
+        {props.children}
+      </Link>
+    );
+  } else {
+    return <button className={styles.btn}>{props.children}</button>;
+  }
 }
--- pages/events/index.jsx
+++ pages/events/index.jsx
@@ -1,12 +1,21 @@
 import { getAllEvents } from "../../dummy-data";
 import EventList from "../../components/events/EventList";
+import EventsSearch from "../../components/events/EventsSearch";
+import { useRouter } from "next/router";
 
 export default function EventsPage() {
+  const router = useRouter();
   const events = getAllEvents();
 
+  function findEventsHandler(year, month) {
+    const fullPath = `/events/${year}/${month}`
+    router.push(fullPath);
+  }
+
   return (
-    <div>
+    <>
+      <EventsSearch onSearch={findEventsHandler} />
       <EventList items={events} />
-    </div>
+    </>
   );
 }
