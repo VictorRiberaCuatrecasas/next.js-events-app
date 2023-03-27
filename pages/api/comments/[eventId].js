6c92e78faeb583cc6213c40b1daf1bf541b8810e
Victor <victorribera92@gmail.com>
2023-03-27 19:02:38 +0200

    db error handling

--- pages/api/comments/[eventId].js
+++ pages/api/comments/[eventId].js
@@ -1,5 +1,18 @@
 import { MongoClient } from "mongodb";
 
+async function connectDatabase() {
+  const client = await MongoClient.connect(
+    `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.m2wd64e.mongodb.net/events?retryWrites=true&w=majority`
+  );
+
+  return client;
+}
+
+async function insertDocument(client, document) {
+  const db = client.db();
+  await db.collection("comments").insertOne(document);
+}
+
 export default async function handler(req, res) {
   const id = req.query.eventId;
 
@@ -21,6 +34,23 @@ export default async function handler(req, res) {
       text,
       id,
     };
+
+    let client;
+
+    try {
+      client = await connectDatabase();
+    } catch (error) {
+      res.status(500).json({message: "failed to connect to db"})
+      return
+    }
+
+    try {
+      await insertDocument(client, newComment);
+      client.close();
+    } catch (error) {
+      res.status(500).json({message: "failed to insert data"})
+      return
+    }
 
     const db = client.db();
     const result = await db.collection("comments").insertOne(newComment);
--- pages/api/newsletterRegistration.js
+++ pages/api/newsletterRegistration.js
@@ -1,5 +1,18 @@
 import { MongoClient } from "mongodb";
 
+async function connectDatabase() {
+  const client = await MongoClient.connect(
+    `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.m2wd64e.mongodb.net/events?retryWrites=true&w=majority`
+  );
+
+  return client;
+}
+
+async function insertDocument(client, document) {
+  const db = client.db();
+  await db.collection("newsletter").insertOne(document);
+}
+
 export default async function handler(req, res) {
   if (req.method === "POST") {
     const userEmail = req.body.email;
@@ -8,12 +21,23 @@ export default async function handler(req, res) {
       res.status(422).json({ message: "invalid email" });
       return;
     }
-    const client = await MongoClient.connect(
-      `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.m2wd64e.mongodb.net/events?retryWrites=true&w=majority`
-    );
-    const db = client.db();
-    await db.collection("newsletter").insertOne({ email: userEmail });
-    client.close();
+
+    let client;
+
+    try {
+      client = await connectDatabase();
+    } catch (error) {
+      res.status(500).json({message: "failed to connect to db"})
+      return
+    }
+
+    try {
+      await insertDocument(client, { email: userEmail });
+      client.close();
+    } catch (error) {
+      res.status(500).json({message: "failed to insert data"})
+      return
+    }
 
     res.status(201).json({ message: "email added to newsletter" });
   }
